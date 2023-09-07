8e7fa7b4454e01dc747a5b8c5b23480754884728
Anirudha Gandhare <anirudha@chisellabs.com>
2023-09-07 09:27:52 +0530

    feat: convert the app to pwa

---
+++ apps/client/manifest.js
@@ -0,0 +1,40 @@
+export const manifest = {
+    registerType: 'prompt',
+    includeAssests: ['favicon.ico', "apple-touch-icon.png", "masked-icon.svg"],
+    manifest: {
+        name: "Moneybolt",
+        short_name: "Moneybolt",
+        description: "Gain Financial Clarity and track expenses with moneybolt.",
+        icons: [{
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'favicon'
+        },
+        {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'favicon'
+        },
+        {
+            src: '/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'apple touch icon',
+        },
+        {
+            src: '/maskable_icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+        }
+        ],
+        theme_color: '#2563EB',
+        background_color: '#fff',
+        display: "standalone",
+        scope: '/',
+        start_url: "/",
+        orientation: 'portrait'
+    }
+}
--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA } from "vite-plugin-pwa";
+import { manifest } from './manifest';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths({
     parseNative: false,
-  })],
+  }), VitePWA(manifest)],
   server: {
     port: 3000,
     proxy: {
